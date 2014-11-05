7fdcc6fdb5b7d365bde71db675b87e4dfdfe9458
nl02032 <michielgkalkman@gmail.com>
2014-11-05 13:15:19 +0100

    Use correct code :(

--- src/main/java/org/taHjaj/wo/pmdplus/dup/DupRule.java
+++ src/main/java/org/taHjaj/wo/pmdplus/dup/DupRule.java
@@ -1,84 +1,110 @@
 package org.taHjaj.wo.pmdplus.dup;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTAnnotation;
+import net.sourceforge.pmd.lang.java.ast.ASTArguments;
+import net.sourceforge.pmd.lang.java.ast.ASTMarkerAnnotation;
+import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTName;
-import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
-import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
+import net.sourceforge.pmd.lang.java.ast.ASTNameList;
+import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
 import net.sourceforge.pmd.lang.java.ast.AbstractJavaTypeNode;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jaxen.JaxenException;
 
-
 public class DupRule extends AbstractJavaRule {
-    @Override
-	public Object visit(final ASTVariableDeclaratorId node, final Object data) {
-    	dump(node);
-    	
-    	final String nameDeclaration = node.getNameDeclaration().getName();
-    	
-    	final Map<String, List<Integer>> image2LinesNummbers = new HashMap<String, List<Integer>>();
-    	
-    	try {
-			final List findChildNodesWithXPath = node.findChildNodesWithXPath( "//Name");
-			for( final Object child : findChildNodesWithXPath) {
-				final ASTName childNode = (ASTName) child;
-				
-				final String image = childNode.getImage();
-				
-				if( image.startsWith(nameDeclaration) && ! image.equals( nameDeclaration)) {
-					
-					List<Integer> list = image2LinesNummbers.get( image);
-					
-					if( list == null) {
-						list = new ArrayList<Integer>();
-						image2LinesNummbers.put(image, list);
+
+	@Override
+	public Object visit(final ASTMethodDeclaration node, final Object data) {
+		try {
+
+			final Map<String, List<Integer>> image2LinesNummbers = new HashMap<String, List<Integer>>();
+
+			final List<ASTName> astNames = node
+					.findDescendantsOfType(ASTName.class);
+
+			for (final ASTName astName : astNames) {
+				final String image = astName.getImage();
+
+				final Node parent = astName.getNthParent(1);
+				final Node grandParent = astName.getNthParent(2);
+
+				if (!(parent instanceof ASTNameList)
+						&& !(grandParent instanceof ASTAnnotation)
+						&& !(parent instanceof ASTMarkerAnnotation)) {
+
+					final ASTPrimaryExpression astPrimaryExpression = (ASTPrimaryExpression) astName
+							.getNthParent(2);
+
+					List arguments;
+					arguments = astPrimaryExpression
+							.findChildNodesWithXPath("PrimarySuffix/Arguments");
+
+					if (!arguments.isEmpty()) {
+						final ASTArguments astArguments = (ASTArguments) arguments
+								.get(0);
+
+						if (astArguments.getArgumentCount() == 0) {
+							final String name = image.contains(".") ? StringUtils
+									.substringBefore(image, ".") : image;
+
+							// final List variableDeclaratorId = node
+							// .findChildNodesWithXPath(String
+							// .format("//VariableDeclaratorId[@Image='%s']",
+							// name));
+
+							// if (!variableDeclaratorId.isEmpty()) {
+							List<Integer> list = image2LinesNummbers.get(image);
+
+							if (list == null) {
+								list = new ArrayList<Integer>();
+								image2LinesNummbers.put(image, list);
+							}
+
+							list.add(astName.getBeginLine());
+							// }
+						}
 					}
-					
-					list.add( childNode.getBeginLine());
 				}
 			}
-			
-			for( final Map.Entry<String, List<Integer>> entry : image2LinesNummbers.entrySet()) {
-				if( entry.getValue().size() > 1) {
-		            addViolation(data, node);
+
+			for (final Map.Entry<String, List<Integer>> entry : image2LinesNummbers
+					.entrySet()) {
+				if (entry.getValue().size() > 1) {
+					final String lines = StringUtils
+							.join(entry.getValue(), ",");
+					addViolation(data, node, new String[] { entry.getKey(),
+							lines });
 				}
 			}
-			
-		} catch (final JaxenException e) {
+		} catch (final JaxenException e1) {
 			// TODO Auto-generated catch block
-		
-			e.printStackTrace();
+			e1.printStackTrace();
+		} catch (final ClassCastException e1) {
+			final String x = ((RuleContext) data).getSourceCodeFilename() + ":"
+					+ node.getBeginLine();
+
+			throw new RuntimeException(x, e1);
 		}
-    	
-    	
-    	return super.visit(node, data);
-	}
-	
-    @Override
-	public Object visit(final ASTPrimaryPrefix node, final Object data) {
-    	dump(node);
-		return super.visit(node, data);
-	}
-	
-    @Override
-	public Object visit(final ASTName node, final Object data) {
-    	dump(node);
+
 		return super.visit(node, data);
 	}
 
 	private void dump(final AbstractJavaTypeNode node) {
 		final StringBuilder stringBuilder = new StringBuilder();
-    	stringBuilder.append(node.getClass().getSimpleName())
-    		.append( "=================================================================\nScope:")
-    		.append( node.getScope())
-    		.append( "\nImage:")
-    		.append( node.getImage())
-    		.append( "\n");
-    	System.out.println( stringBuilder);
+		stringBuilder
+				.append(node.getClass().getSimpleName())
+				.append("=================================================================\nScope:")
+				.append(node.getScope()).append("\nImage:")
+				.append(node.getImage()).append("\n");
+		System.out.println(stringBuilder);
 	}
 }
-    
--- src/test/java/org/taHjaj/wo/pmdplus/dup/DupTest.java
+++ src/test/java/org/taHjaj/wo/pmdplus/dup/DupTest.java
@@ -5,12 +5,19 @@ package org.taHjaj.wo.pmdplus.dup;
 
 import net.sourceforge.pmd.testframework.SimpleAggregatorTst;
 
+import org.junit.Before;
+
+
 public class DupTest extends SimpleAggregatorTst {
 
     private static final String RULESET = "java-dup";
 
-    @Override
+    @Before
     public void setUp() {
         addRule(RULESET, "Dup");
+    }
+    
+    public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(DupTest.class);
     }
 }

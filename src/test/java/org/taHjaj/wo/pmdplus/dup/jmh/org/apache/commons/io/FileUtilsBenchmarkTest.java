7bd0af3f4c6a6af57e92522e9b101ff012d6846d
Michiel Kalkman <michiel.kalkman@atos.net>
2020-10-08 20:07:45 +0200

    added test results

--- src/test/java/org/taHjaj/wo/pmdplus/dup/jmh/org/apache/commons/io/FileUtilsBenchmarkTest.java
+++ src/test/java/org/taHjaj/wo/pmdplus/dup/jmh/org/apache/commons/io/FileUtilsBenchmarkTest.java
@@ -2,6 +2,7 @@ package org.taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io;
 
 import org.junit.Test;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.profile.WinPerfAsmProfiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
@@ -21,6 +22,49 @@ import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class FileUtilsBenchmarkTest {
+    //
+    // Test tiny improvement to FileUtils::opyDirectory in
+    // commons-io:commons-io:2.9.0-SNAPSHOT.
+    //
+    // I just removed duplicate call to File::getCanonicalPath.
+    //
+    // Would it show when using jmh for testing?
+    //
+    // I ran this code on my Dell Latitude E5550 laptop using Windows 10 
+    // 64-bit with 16GB memory and an Intel(R) Core(TM) i7-5600 CPU @ 2.60GHz
+    // using a LITEONIT LCS-256L9S-11 2.5 7mm 256GB disk drive
+    // using JDK:
+    // openjdk version "11.0.8" 2020-07-14 LTS
+    // OpenJDK Runtime Environment Corretto-11.0.8.10.1 (build 11.0.8+10-LTS)
+    // OpenJDK 64-Bit Server VM Corretto-11.0.8.10.1 (build 11.0.8+10-LTS, mixed mode)
+    //
+    // Results from two test runs:
+    //
+    //    Benchmark                                                                                  Mode  Cnt        Score       Error  Units
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.arunBenchmark       avgt  100    11083,590 ±   768,848  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.arunBenchmark1by1   avgt  100    51988,380 ± 11676,741  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.arunBenchmark2by2   avgt  100   204613,482 ± 37215,888  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.arunBenchmark3by3   avgt  100  1174152,673 ± 22541,114  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.dedupBenchmark1by1  avgt  100    31088,342 ±  1264,120  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.dedupBenchmark2by2  avgt  100   146071,467 ±  3787,178  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.dedupBenchmark3by3  avgt  100  1154023,563 ± 15089,337  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.deduppedBenchmark   avgt  100    14246,154 ±  2367,264  us/op
+    //
+    //    Benchmark                                                                                  Mode  Cnt        Score       Error  Units
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.arunBenchmark       avgt  100    11273,827 ±  2165,753  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.arunBenchmark1by1   avgt  100    32702,611 ±  1813,470  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.arunBenchmark2by2   avgt  100   152587,381 ±  4642,588  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.arunBenchmark3by3   avgt  100  1195478,564 ± 24303,450  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.dedupBenchmark1by1  avgt  100    30884,652 ±  1310,080  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.dedupBenchmark2by2  avgt  100   147020,471 ±  4613,101  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.dedupBenchmark3by3  avgt  100  1185322,846 ± 24429,620  us/op
+    //    taHjaj.wo.pmdplus.dup.jmh.org.apache.commons.io.FileUtilsBenchmarkTest.deduppedBenchmark   avgt  100    11565,818 ±  1937,442  us/op
+    //
+    // On the whole, the deduppedBenchmarks seem to perform better, but I am definitely not an expert on this.
+    //
+    // In partcularly I cannot explain why the deduppedBenchmark benchmark seems to be slower than the arunBenchmark.
+    //
+
     @Test
     public void testCopyDirectories() throws Exception {
         Options opt = new OptionsBuilder()
@@ -38,8 +82,6 @@ public class FileUtilsBenchmarkTest {
                 .forks(1)
                 .shouldFailOnError(true)
                 .shouldDoGC(false)
-                //.jvmArgs("-XX:+UnlockDiagnosticVMOptions", "-XX:+PrintInlining")
-                //.addProfiler(WinPerfAsmProfiler.class)
                 .build();
 
         new Runner(opt).run();

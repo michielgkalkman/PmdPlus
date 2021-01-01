77925501b75d146413dd0fd23ae37d565483d051
Michiel Kalkman <michiel.kalkman@atos.net>
2021-01-01 20:21:18 +0100

    Finding common expressions for if statements

--- src/main/java/org/taHjaj/wo/pmdplus/dup/DupIfRule.java
+++ src/main/java/org/taHjaj/wo/pmdplus/dup/DupIfRule.java
@@ -1,20 +1,11 @@
 package org.taHjaj.wo.pmdplus.dup;
 
-import net.sourceforge.pmd.RuleContext;
-import net.sourceforge.pmd.lang.ast.AbstractNode;
 import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.lang.java.ast.*;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
-import net.sourceforge.pmd.lang.java.typeresolution.typedefinition.JavaTypeDefinition;
-import net.sourceforge.pmd.lang.symboltable.NameDeclaration;
 import org.apache.commons.lang3.StringUtils;
-import org.jaxen.JaxenException;
-import org.jaxen.expr.RelationalExpr;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.*;
-import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class DupIfRule extends AbstractJavaRule {
@@ -23,28 +14,24 @@ public class DupIfRule extends AbstractJavaRule {
 
         // Determine the expression in the if statement.
         // Determine the expressions in the branches
-        ASTExpression ifExpression = null;
-        Map<Node, List<ASTExpression>> ifExpressions = new HashMap<>();
-        Map<Node, List<ASTExpression>> branchExpressions = new HashMap<>();
+        JavaNode ifExpression;
+        List<JavaNode> ifExpressions = new ArrayList<>();
+        Map<Node, List<JavaNode>> branchExpressions = new HashMap<>();
 
-        for( final Node childNode : node.children() ) {
-            if( childNode instanceof ASTExpression) {
-                ifExpression = (ASTExpression) childNode;
-                final List<ASTExpression> astExpressionList = childNode
-                        .findDescendantsOfType(ASTExpression.class);
-                astExpressionList.add(ifExpression);
-                ifExpressions.put(childNode, astExpressionList);
-            } else {
-                final List<ASTExpression> astExpressionList = childNode
-                        .findDescendantsOfType(ASTExpression.class);
-                branchExpressions.put( childNode, astExpressionList);
-            }
+
+        ifExpression = node.getChild(0);
+        ifExpressions = findAllExpressions(ifExpression);
+
+        ifExpressions.forEach(javaNode -> System.out.printf( "ifExpression: %s%n", toString(javaNode)));
+
+        for( int i=1; i<node.getNumChildren(); i++) {
+            final JavaNode childNode = node.getChild(i);
+            final List<JavaNode> astExpressionList = findAllExpressions(childNode);
+            branchExpressions.put( childNode, astExpressionList);
         }
 
         // Now check if there is any expression in the branches that is compatible
         // with the ifExpression
-
-        boolean fFound = false;
 
 
         reportDuplicates(ifExpressions, branchExpressions, data);
@@ -63,33 +50,61 @@ public class DupIfRule extends AbstractJavaRule {
         return super.visit(node, data);
     }
 
-    private void reportDuplicates(
-            Map<Node, List<ASTExpression>> ifExpressions,
-            Map<Node, List<ASTExpression>> branchExpressions, Object data) {
-        ifExpressions.entrySet().forEach(ifExpression -> {
-            for( ASTExpression ifE : ifExpression.getValue()) {
-                branchExpressions.entrySet().stream().flatMap(e -> e.getValue().stream()).forEach(statementExpression -> {
-                    final String ifExpressionString = toString(ifE);
-                    final String statementExpressionString = toString(statementExpression);
-                    System.out.printf("Compare %s with %s%n", ifExpressionString, statementExpressionString);
-                    if (ifExpressionString.equals(statementExpressionString)) {
-                        System.out.printf("Equal %s with %s%n", ifExpressionString, statementExpressionString);
-                        addViolation(ifE, data, Arrays.asList(ifE, statementExpression), ifE.getImage());
-                    } else {
-                        System.out.printf("Not equal %s with %s%n", ifExpressionString, statementExpressionString);
-                    }
-                });
+    private List<JavaNode> findAllExpressions(JavaNode node) {
+        final List<JavaNode> astExpressionList = node
+                .findDescendantsOfType(ASTExpression.class);
+
+        final List<ASTRelationalExpression> astRelationalExpressions =
+                node.findDescendantsOfType(ASTRelationalExpression.class);
+
+        for( ASTRelationalExpression astRelationalExpression : astRelationalExpressions) {
+            astExpressionList.addAll(findAllExpressions(astRelationalExpression.getChild(0)));
+            astExpressionList.addAll(findAllExpressions(astRelationalExpression.getChild(1)));
+        }
+
+        if(isExpression(node)) {
+            astExpressionList.add( node);
+        } else if( node instanceof ASTPrimaryExpression) {
+            if( node.getNumChildren() == 2
+                    && node.getChild(0) instanceof ASTPrimaryPrefix
+                    && node.getChild(1) instanceof ASTPrimarySuffix) {
+                astExpressionList.add(node);
+                astExpressionList.addAll(findAllExpressions(node.getChild(1)));
             }
-        });
+
+        }
+        return astExpressionList;
     }
 
-    private String toString(ASTExpression astExpression) {
+    private boolean isExpression(Node node) {
+
+
+        return node instanceof ASTExpression
+                || node instanceof ASTAdditiveExpression;
+    }
+
+    private void reportDuplicates(
+            List<JavaNode> ifExpressions,
+            Map<Node, List<JavaNode>> branchExpressions, Object data) {
+        ifExpressions.forEach(ifE -> branchExpressions.entrySet().stream().flatMap(e -> e.getValue().stream()).forEach(statementExpression -> {
+            final String ifExpressionString = toString(ifE);
+            final String statementExpressionString = toString(statementExpression);
+            System.out.printf("Compare %s with %s%n", ifExpressionString, statementExpressionString);
+            if (ifExpressionString.equals(statementExpressionString)) {
+                System.out.printf("Equal %s with %s%n", ifExpressionString, statementExpressionString);
+                addViolation(ifE, data, Arrays.asList(ifE, statementExpression), ifE.getImage());
+            } else {
+                System.out.printf("Not equal %s with %s%n", ifExpressionString, statementExpressionString);
+            }
+        }));
+    }
+
+    private String toString(JavaNode astExpression) {
         StringBuilder stringBuilder = new StringBuilder();
 
         toString(stringBuilder, astExpression);
 
-        final String s = stringBuilder.toString();
-        return s;
+        return stringBuilder.toString();
     }
 
     private void toString(StringBuilder stringBuilder, JavaNode javaNode) {
@@ -126,11 +141,10 @@ public class DupIfRule extends AbstractJavaRule {
         }
     }
 
-
-    private void addViolation(final ASTExpression node, final Object data, final List<ASTExpression> astExpressions,
+    private void addViolation(final JavaNode node, final Object data, final List<JavaNode> astExpressions,
             final String image) {
         final String lines = StringUtils
-                .join(astExpressions.stream().map(AbstractNode::getBeginLine).collect(Collectors.toList()), ",");
+                .join(astExpressions.stream().map(Node::getBeginLine).collect(Collectors.toList()), ",");
         addViolation(data, node, new String[] {image,
                 lines });
     }
